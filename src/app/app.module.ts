9344fadc5e099dc5b2a77f3702568259529cdbc5
Mehdi Noroozi <mehdi.noroozi@qhrtech.com>
2022-03-30 11:40:30 -0600

    configure zipkin exporter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  ZipkinExporterModule,
+  CompositePropagatorModule,
+  OtelWebTracerModule,
+} from '@jufab/opentelemetry-angular-interceptor';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ZipkinExporterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //Insert propagator module
+    CompositePropagatorModule,
+    OtelWebTracerModule.forRoot(environment.openTelemetryConfig),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,42 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { DiagLogLevel } from '@opentelemetry/api';
+import {
+  OpenTelemetryConfig
+} from '@jufab/opentelemetry-angular-interceptor';
 
-export const environment = {
-  production: false
+interface IEnvironment {
+  production: boolean;
+  urlTest: string;
+  openTelemetryConfig: OpenTelemetryConfig;
+}
+
+// Example to configure the angular-interceptor library
+export const environment: IEnvironment = {
+  production: false,
+  urlTest: 'http://localhost:4200',
+  openTelemetryConfig: {
+    commonConfig: {
+      console: true, // Display trace on console
+      production: true, // Send Trace with BatchSpanProcessor (true) or SimpleSpanProcessor (false)
+      serviceName: 'instrumentation-example', // Service name send in trace
+      probabilitySampler: '1', // 75% sampling
+      logLevel: DiagLogLevel.ALL //ALL Log, DiagLogLevel is an Enum from @opentelemetry/api
+    },
+    zipkinConfig: {
+      url: 'http://localhost:9411/api/v2/spans'
+    },
+    b3PropagatorConfig: {
+      multiHeader: '0' //Value : 'O' (single), '1' (multi)
+    },
+    instrumentationConfig: {
+      xmlHttpRequest: true,
+      fetch: true,
+      documentLoad: true,
+    }
+  }
 };
+
+// import { ZipkinCollectorConfig } from '../../../../dist/opentelemetry-interceptor/lib/configuration/opentelemetry-config';
+//import { zipkinConfig } from '../../../opentelemetry-interceptor/__mocks__/data/config.mock';
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -13,4 +45,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
